52fd6b0869580ce4546038e2112690e82e321f4b
Kyu♥ <3ad4gum@gmail.com>
2024-03-03 22:27:49 +0100

    showing up now, but functionality is not being replaced by inject somehow...

--- slutty-joker.lua
+++ slutty-joker.lua
@@ -1,8 +1,32 @@
-patched = false
+patchedSlutty = false
 centerHook = initCenterHook()
 
 if (sendDebugMessage == nil) then
     sendDebugMessage = function(_)
+    end
+end
+
+function input(path, function_name, to_replace, replacement)
+    -- Injects code into a function (replaces a string with another string inside a function)
+    local function_body = extractFunctionBody(path, function_name)
+    local modified_function_code = function_body:gsub(to_replace, replacement)
+    current_game_code[path] = current_game_code[path]:gsub(function_body, modified_function_code) -- update current game code in memory
+
+    local new_function, load_error = load(modified_function_code) -- load modified function
+    if not new_function then
+        -- Safeguard against errors, will be logged in %appdata%/Balatro/err1.txt
+        love.filesystem.write("err1.txt", "Error loading modified function: " .. (load_error or "Unknown error"))
+    end
+
+    if setfenv then
+        setfenv(new_function, getfenv(original_testFunction))
+    end -- Set the environment of the new function to the same as the original function
+
+    local status, result = pcall(new_function) -- Execute the new function
+    if status then
+        testFunction = result -- Overwrite the original function with the result of the new function
+    else
+        love.filesystem.write("err2.txt", "Error executing modified function: " .. result) -- Safeguard against errors, will be logged in %appdata%/Balatro/err2.txt
     end
 end
 
@@ -17,42 +41,83 @@ table.insert(mods,
             },
             enabled = true,
             on_post_update = function() 
-                if not patched then 
+                if not patchedSlutty then 
                     centerHook.addJoker(self, "j_slutty", "Slutty Joker", nil, true, 1, { x = 0, y = 9 }, nil, {extra = 2}, { "{X:red,C:white} X2 {} Mult", "if played hand contains", "a 6 and a 9"}, 1, true)
             
-                    -- local replacement = [[local used_tarot = copier or self
-                    
-                    -- if self.ability.name == "Bezos" then
-                    --     G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
-                    --         play_sound('timpani')
-                    --         ease_dollars(100, true)
-                    --         return true end }))
-                    --     delay(0.6)
-                    -- end
-                    -- ]]
-                    -- local to_replace = [[local used_tarot = copier or self]]
-                    -- local fun_name = "Card:use_consumeable"
-                    -- local file_name = "card.lua"
-                    -- inject(file_name, fun_name, to_replace, replacement)
-        
-                    -- local to_replace = [[if self.ability.name == 'The Hermit' or self.ability.consumeable.hand_type or self.ability.name == 'Temperance' or self.ability.name == 'Black Hole' then]]
-                    -- local replacement = [[sendDebugMessage(self)
-                    -- if self.ability.name == 'The Hermit' or self.ability.consumeable.hand_type or self.ability.name == 'Temperance' or self.ability.name == 'Black Hole' or self.ability.name == 'Bezos' then]]
-                    -- local fun_name = "Card:can_use_consumeable"
-                    -- inject(file_name, fun_name, to_replace, replacement)
-
-                    toPatch = "if _center.name == 'Photograph' and (_center.discovered or self.bypass_discovery_center) then "
-
-                    patch = [[
-                        if _center.name == 'Photograph' and (_center.discovered or self.bypass_discovery_center) then 
-                            self.children.center.scale.y = self.children.center.scale.y/1.2
+                    local toReplace = [[if self.ability.name == '8 Ball' and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                        local eights = 0
+                        for i = 1, #context.full_hand do
+                            if context.full_hand[i]:get_id() == 8 then eights = eights + 1 end
                         end
-                        if _center.name == 'Slutty Joker'
-                            self.
+                        if eights >= self.ability.extra then
+                            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                            G.E_MANAGER:add_event(Event({
+                                trigger = 'before',
+                                delay = 0.0,
+                                func = (function()
+                                        local card = create_card('Planet',G.consumeables, nil, nil, nil, nil, nil, '8ba')
+                                        card:add_to_deck()
+                                        G.consumeables:emplace(card)
+                                        G.GAME.consumeable_buffer = 0
+                                    return true
+                                end)}))
+                            return {
+                                message = localize('k_plus_planet'),
+                                colour = G.C.SECONDARY_SET.Planet,
+                                card = self
+                            }
                         end
+                    end
                     ]]
 
-                    patched = true
+                    local replacement = [[if self.ability.name == '8 Ball' and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                        local eights = 0
+                        for i = 1, #context.full_hand do
+                            if context.full_hand[i]:get_id() == 8 then eights = eights + 1 end
+                        end
+                        if eights >= self.ability.extra then
+                            G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+                            G.E_MANAGER:add_event(Event({
+                                trigger = 'before',
+                                delay = 0.0,
+                                func = (function()
+                                        local card = create_card('Planet',G.consumeables, nil, nil, nil, nil, nil, '8ba')
+                                        card:add_to_deck()
+                                        G.consumeables:emplace(card)
+                                        G.GAME.consumeable_buffer = 0
+                                    return true
+                                end)}))
+                            return {
+                                message = localize('k_plus_planet'),
+                                colour = G.C.SECONDARY_SET.Planet,
+                                card = self
+                            }
+                        end
+                    end
+
+                    if self.ability.name == 'Slutty Joker' and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
+                        local sixs = 0
+                        local nines = 0
+
+                        for i = 1, #context.full_hand do
+                            if context.full_hand[i]:get_id() == 6 then sixs = sixs + 1 end
+                            if context.full_hand[i]:get_id() == 9 then nines = nines + 1 end
+                        end
+                        if sixs > 0 and nines > 0 then
+                            return {
+                                message = localize{type='variable',key='a_xmult',vars={self.ability.extra}},
+                                Xmult_mod = self.ability.extra
+                            }
+                        end
+                    end
+                    ]]
+                    
+                    input("card.lua", "Card:calculate_joker", toReplace:gsub("([^%w])", "%%%1"), replacement)
+
+                    local function_body = extractFunctionBody("card.lua", "Card:calculate_joker")
+                    sendDebugMessage(function_body)
+
+                    patchedSlutty = true
                     sendDebugMessage("Patched slutty joker mod !")
                 end
             end,
